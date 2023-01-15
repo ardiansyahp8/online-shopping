3c881d88d9ae9c3d946d60a5237421418d8be5e8
ardiansyahp8 <ardiansyahp8@gmail.com>
2023-01-15 16:19:49 +0700

    get products data on homepage frontend

---
+++ controllers/frontend.controller.js
@@ -0,0 +1,28 @@
+const db = require('../models/bundle.model')
+const Op = db.Sequelize.Op
+const func = require('../libs/function')
+const {v4: uuidv4} = require('uuid')
+
+exports.getProdukHome = async (req, res) => {
+    db.produk.findAll({
+        limit: 8
+    }).then(result => {
+        if (result.length > 0){
+            res.send({
+                code: 200,
+                message: 'OK',
+                data: result
+            })
+        } else {
+            res.status(404).send({
+                code: 404,
+                message: 'Data tidak tersedia'
+            })
+        }
+    }).catch(err => {
+        res.status(500).send({
+            code: 500,
+            message: 'Error Find Data > ' + err
+        })
+    })
+}
---
+++ route/frontend.js
@@ -0,0 +1,7 @@
+const express = require('express')
+const router = express.Router()
+const frontend = require('../controllers/frontend.controller')
+
+
+
+module.exports = router
