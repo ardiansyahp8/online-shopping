c4f12b72976facb6c5e847f21f121e3cba7d1de4
M. Ardiansyah Pratama <ardiansyahp8@gmail.com>
2023-01-22 23:12:10 +0700

    product specification in detail

--- controllers/frontend.controller.js
+++ controllers/frontend.controller.js
@@ -58,6 +58,9 @@ exports.getProdukPage = async (req, res) => {
     })
 }
 
-exports.getProdukPage = async (req, res) => {
-    const id = req.params.id;
+exports.getProdukDetil = async (req, res) => {
+    const id = req.params.id
+
+    db.Produk.detail({
+    })
 }
