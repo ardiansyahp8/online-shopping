88b07684349d61d5b25c775e683227b8cca45a02
ardiansyahp8 <ardiansyahp8@gmail.com>
2023-02-01 07:30:10 -0600

    customization change

--- app.js
+++ app.js
@@ -17,7 +17,5 @@ app.use('/kategori', kategoriRouter)
 
 const frontendRouter = require('./route/frontend')
 app.use('/frontend', frontendRouter)
-const backendRouter = require('./route/backend')
-app.use('/backend', backendRouter)
 
 module.exports = app
