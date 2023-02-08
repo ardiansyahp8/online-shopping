dbe91cef82c37cc09e33ee97f6625d72a6af32b0
ardiansyahp8 <ardiansyahp8@gmail.com>
2023-02-08 17:05:10 +0700

    modifying schema

--- validation/produk/produk.schema.js
+++ validation/produk/produk.schema.js
@@ -5,6 +5,7 @@ const schema = {
         title: Joi.string().max(150).required(),
         description: Joi.string().max(255).allow(''),
         full_description: Joi.string().max(5000).allow(''),
+        source: Joi.string().max(255).allow(''),
         price: Joi.number().min(100).max(100000000).required(),
         category_id: Joi.number().required()
     })
